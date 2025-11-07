90b1399d49c5fd0fdbbb2dd9daa56fed2f505b29
Timothy Wang <2556936w@student.gla.ac.uk>
2025-11-07 00:42:27 +0000

    add n5 notes for pupil file testing

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import os
 from django.shortcuts import render
 from django.http import FileResponse
+from django.conf import settings
 from .utils import generate_text, create_quiz_doc
 
 # Home page
@@ -42,3 +44,36 @@ def quiz_gen(request):
         return FileResponse(open(filepath, "rb"), as_attachment=True, filename="generated-quiz.docx")
     
     return render(request, "app/teacher/quiz_gen.html")
+
+# Pupil resources
+def pupil_resources(request):
+    base_path = os.path.join(settings.STATIC_ROOT or settings.STATICFILES_DIRS[0], 'app/pupil_resources')
+    folders = {}
+
+    for root, dirs, files in os.walk(base_path):
+        rel_path = os.path.relpath(root, base_path)
+        if rel_path == ".":
+            folder_name = "Root"
+        else:
+            folder_name = rel_path
+        file_paths = [
+            os.path.join('pupil_resources', rel_path, f).replace('\\', '/')
+            for f in files
+        ]
+        if files:
+            folders[folder_name] = file_paths
+        
+        # File info and sizes
+        file_info = []
+        for f in files:
+            abs_path = os.path.join(root, f)
+            size_kb = round(os.path.getsize(abs_path) / 1024, 1)
+            file_info.append({
+                "path": os.path.join('pupil_resources', rel_path, f).replace('\\', '/'),
+                "name": f,
+                "size": size_kb,
+            })
+        if file_info:
+            folders[folder_name] = file_info
+
+    return render(request, "app/pupil/resources.html", {"folders": folders})
