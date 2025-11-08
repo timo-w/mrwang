2c85e05650bea465b4acb12d40687531b02feb8a
Timothy Wang <timothy.wang.me.uk@gmail.com>
2025-11-08 15:51:36 +0000

    Add modal overlays to pupil and teacher pages

--- app/static/app/js/global.js
+++ app/static/app/js/global.js
@@ -2,4 +2,29 @@
 
 $(document).ready(function(){
     console.log("Global script loaded!");
-  });
+
+    const $modal = $('#appModal');
+
+  // Open modal
+    $('.grid-button').on('click', function() {
+        $('#modalTitle').text($(this).data('app-name'));
+        $('#modalDesc').text($(this).data('app-desc'));
+        $('#modalImg').attr('src', $(this).data('app-img'));
+        $('#modalLink').attr('href', $(this).data('app-link'));
+
+        $modal.addClass('show');
+    });
+
+    // Close modal (X button)
+    $('.close-modal').on('click', function() {
+        $modal.removeClass('show');
+    });
+
+    // Close modal by clicking outside
+    $modal.on('click', function(e) {
+        if ($(e.target).is($modal)) {
+        $modal.removeClass('show');
+        }
+    });
+
+});
