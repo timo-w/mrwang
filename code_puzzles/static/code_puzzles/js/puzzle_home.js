5f0cf058c894adf053df530f192de58a7db63fde
Timothy Wang <timothy.wang.me.uk@gmail.com>
2025-11-23 15:57:30 +0000

    add program description to examples, style modal to fit larger programs

--- code_puzzles/static/code_puzzles/js/puzzle_home.js
+++ code_puzzles/static/code_puzzles/js/puzzle_home.js
@@ -7,12 +7,14 @@ $(document).ready(function() {
     const modal = $("#appModal");
     const modalTitle = $("#modalTitle");
     const modalTopics = $("#modalTopics");
+    const modalDescription = $("#modalDescription");
     const modalCode = $("#modalCode");
 
     // Open modal when clicking a grid button
     $(".grid-button-small").on("click", function() {
         const title = $(this).data("title");
         const topics = $(this).data("topics");
+        const description = $(this).data("description")
 
         // Read the code from the hidden <pre>
         let code = $(this).find(".program-code").text().replace(/\\r/g, "");
@@ -21,6 +23,7 @@ $(document).ready(function() {
 
         modalTitle.text(title);
         modalTopics.text(topics || "None");
+        modalDescription.text(description);
         modalCode.html(code);
         
         // Re-highlight using Prism
