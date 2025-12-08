de8945437606f06e7fc536935f84f5324892ebee
Timothy Wang <timothy.wang.me.uk@gmail.com>
2025-12-08 00:23:58 +0000

    Added functionality to music page

--- music/models.py
+++ music/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Composition(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.PositiveIntegerField()
+    description = models.TextField()
+    youtube_link = models.URLField()
+    sheet_music = models.FileField(upload_to="sheet_music/", blank=True, null=True) 
+
+    class Meta:
+        ordering = ["-year", "name"]  # newest first, then alphabetical
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
--- music/views.py
+++ music/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Composition
+
 
 # Music page
 def music(request):
-    return render(request, "music/music.html")
+    compositions = Composition.objects.all()  # already ordered by Meta ordering
+    return render(request, "music/music.html", {"compositions": compositions})
