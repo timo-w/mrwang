621616347183ecc588a78b35bccffb5e16a7982e
Timothy Wang <timothy.wang.me.uk@gmail.com>
2026-08-24 16:56:25 +0100

    Update feedback form appearance

--- feedback/forms.py
+++ feedback/forms.py
@@ -2,9 +2,13 @@ from django import forms
 from .models import Feedback
 
 class FeedbackForm(forms.ModelForm):
+    email = forms.EmailField(
+        required=False,
+        label="Email (optional)"
+    )
     class Meta:
         model = Feedback
         fields = ["category", "message", "email"]
         widgets = {
-            "message": forms.Textarea(attrs={"rows": 5}),
+            "message": forms.Textarea(attrs={"rows": 8}),
         }
