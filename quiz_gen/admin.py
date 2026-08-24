9b4fb80ebd965f45565b999108951cbe7a316f8c
Timothy Wang <timothy.wang.me.uk@gmail.com>
2026-08-24 10:29:07 +0100

    update quiz gen admin view

--- quiz_gen/admin.py
+++ quiz_gen/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import QuizGenerationEvent
 
-admin.site.register(QuizGenerationEvent)
+@admin.register(QuizGenerationEvent)
+class QuizGenerationEventAdmin(admin.ModelAdmin):
+    list_display = (
+        "timestamp",
+        "subject",
+        "topic",
+        "level",
+        "no_of_questions",
+        "no_of_choices",
+        "quiz_type",
+        "file_uploaded",
+    )
+
+    list_filter = ("level", "quiz_type", "file_uploaded", "timestamp")
+    ordering = ("-timestamp",)
