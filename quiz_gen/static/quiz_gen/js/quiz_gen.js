d215fb71060366e7458d6288ea22f17d1fa2a397
Timothy Wang <timothy.wang.me.uk@gmail.com>
2026-08-28 12:05:54 +0100

    Rework quiz gen script to remove duplicated code

--- quiz_gen/static/quiz_gen/js/quiz_gen.js
+++ quiz_gen/static/quiz_gen/js/quiz_gen.js
@@ -1,325 +1,235 @@
-/* Scripts for quiz generator */
+/* Quiz Generator Script */
 
-const formsSlides = [
-    {
-      title: "<a class='generating-text'>Generating quiz</a>",
-      description: "Please allow up to 60 seconds for your quiz to download.<br>This quick guide will show you how to import your quiz in Microsoft Forms and share it with your class.",
-      image: window.formsImages.slide0
-    },
-    {
-      title: "Opening Microsoft Forms (1/4)",
-      description: "First, click <a href='https://forms.office.com/' target='_blank'>here</a> to open Microsoft Forms (opens in a new tab).<br>You will need to be signed in to use the site.",
-      image: window.formsImages.slide1
-    },
-    {
-      title: "Uploading your Quiz File (2/4)",
-      description: "Click 'Quick import' then 'Upload from this device'.<br>Then, select the document which has been downloaded to your device (e.g. 'generated-quiz.docx'). Click 'Quiz' once uploaded.",
-      image: window.formsImages.slide2
-    },
-    {
-      title: "Reviewing your Quiz (3/4)",
-      description: "Once the file has converted, click 'Start review' to view your quiz.<br>You can check, edit, or add/remove questions and answers here.",
-      image: window.formsImages.slide3
-    },
-    {
-      title: "Sharing your Quiz (4/4)",
-      description: "Click on 'Collect responses' to share your quiz.<br>You can share by link, email, or QR code.",
-      image: window.formsImages.slide4
-    },
-];
-let formsSlide = 0
-
-function updateFormsSlide() {
-    $('#modalTitle').html(formsSlides[formsSlide].title);
-    $('#modalDesc').html(formsSlides[formsSlide].description);
-    $('#modalImg').attr('src', formsSlides[formsSlide].image);
-    if (formsSlide == 0) {
-        $('#forms-previous-slide').hide();
-    } else if (formsSlide == (formsSlides.length - 1)) {
-        $('#forms-next-slide').hide();
-    } else {
-        $('#forms-previous-slide').show();
-        $('#forms-next-slide').show();
-    }
-}
-
-
-const blooketSlides = [
-    {
-        title: "<a class='generating-text'>Generating quiz</a>",
-        description: "Please allow up to 60 seconds for your quiz to download.<br>This quick guide will show you how to import your quiz into Blooket.",
-        image: window.blooketImages.slide0
-    },
-    {
-        title: "Opening Blooket (1/5)",
-        description: "First, click <a href='https://www.blooket.com/' target='_blank'>here</a> to open Blooket (opens in a new tab).<br>You will need to be signed in to use the site.",
-        image: window.blooketImages.slide1
-    },
-    {
-        title: "Creating a Question Set (2/5)",
-        description: "Click <strong>Create</strong> and select <strong>CSV Upload</strong>.",
-        image: window.blooketImages.slide2
-    },
-    {
-        title: "Uploading your Quiz (3/5)",
-        description: "Enter the details for your question set and click <strong>Create Set</strong>.",
-        image: window.blooketImages.slide3
-    },
-    {
-        title: "Importing your Questions (4/5)",
-        description: "Click <strong>Upload CSV</strong> and select the Blooket CSV file which has been downloaded to your device. Blooket will import the questions and answers into your question set.",
-        image: window.blooketImages.slide4
-    },
-    {
-        title: "Save your Set (5/5)",
-        description: "Check the imported questions and click <strong>Save Set</strong> when you are done. You should now see your question set in your account!",
-        image: window.blooketImages.slide5
-    }
-];
-let blooketSlide = 0;
-
-function updateBlooketSlide() {
-    $('#blooketModalTitle').html(blooketSlides[blooketSlide].title);
-    $('#blooketModalDesc').html(blooketSlides[blooketSlide].description);
-    $('#blooketModalImg').attr('src', blooketSlides[blooketSlide].image);
-    if (blooketSlide == 0) {
-        $('#blooket-previous-slide').hide();
-    } else {
-        $('#blooket-previous-slide').show();
-    }
-    if (blooketSlide == blooketSlides.length - 1) {
-        $('#blooket-next-slide').hide();
-    } else {
-        $('#blooket-next-slide').show();
-    }
-}
-
-
-const gimkitSlides = [
-    {
-        title: "<a class='generating-text'>Generating quiz</a>",
-        description: "Please allow up to 60 seconds for your quiz to download.<br>This quick guide will show you how to import your quiz into Gimkit.",
-        image: window.gimkitImages.slide0
-    },
-    {
-        title: "Opening Gimkit (1/5)",
-        description: "First, click <a href='https://www.gimkit.com/' target='_blank'>here</a> to open Gimkit (opens in a new tab).<br>You will need to be signed in to use the site.",
-        image: window.gimkitImages.slide1
-    },
-    {
-        title: "Creating a Question Set (2/5)",
-        description: "Click <strong>New Kit</strong>, fill in your <strong>quiz details</strong>, and select a <strong>cover image</strong>.",
-        image: window.gimkitImages.slide2
-    },
-    {
-        title: "Importing your Quiz (3/5)",
-        description: "Click <strong>Import from Spreadsheet</strong>.",
-        image: window.gimkitImages.slide3
-    },
-    {
-        title: "Importing your Quiz (4/5)",
-        description: "Click <strong>Upload File</strong> and select the <strong>Gimkit Quiz</strong> file which has been downloaded to your device. Gimkit will import the questions and answers into your question set.",
-        image: window.gimkitImages.slide4
-    },
-    {
-        title: "Save your Set (5/5)",
-        description: "Check the imported questions and click <strong>All Done</strong> when you are done. You should now see your question set in your account!",
-        image: window.gimkitImages.slide5
-    }
-];
-let gimkitSlide = 0;
-
-function updateGimkitSlide() {
-    $('#gimkitModalTitle').html(gimkitSlides[gimkitSlide].title);
-    $('#gimkitModalDesc').html(gimkitSlides[gimkitSlide].description);
-    $('#gimkitModalImg').attr('src', gimkitSlides[gimkitSlide].image);
-    if (gimkitSlide == 0) {
-        $('#gimkit-previous-slide').hide();
-    } else {
-        $('#gimkit-previous-slide').show();
-    }
-    if (gimkitSlide == gimkitSlides.length - 1) {
-        $('#gimkit-next-slide').hide();
-    } else {
-        $('#gimkit-next-slide').show();
-    }
-}
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("Quiz_gen script loaded!");
 
-    let open = false;
     const $modal = $('#appModal');
 
-    // Open more quiz options
-    $('#more-options').on('click', function() {
-        $('.collapsable').slideToggle();
-        if (open) {
-            open = false;
-            $(this).html("&darr; More options &darr;");
-        } else {
-            open = true;
-            $(this).html("&uarr; Hide options &uarr;");
+    // Instruction slide system
+
+    const slideSets = {
+        forms: window.formsImages,
+        blooket: window.blooketImages,
+        gimkit: window.gimkitImages
+    };
+
+    const slides = {
+        forms: [
+            {
+                title: "<a class='generating-text'>Generating quiz</a>",
+                description: "Please allow up to 60 seconds for your quiz to download.<br>This quick guide will show you how to import your quiz in Microsoft Forms and share it with your class.",
+                image: slideSets.forms.slide0
+            },
+            {
+                title: "Opening Microsoft Forms (1/4)",
+                description: "First, click <a href='https://forms.office.com/' target='_blank'>here</a> to open Microsoft Forms. Sign into your account.",
+                image: slideSets.forms.slide1
+            },
+            {
+                title: "Uploading your Quiz File (2/4)",
+                description: "Click 'Quick import' then 'Upload from this device'. Select your downloaded quiz file.",
+                image: slideSets.forms.slide2
+            },
+            {
+                title: "Reviewing your Quiz (3/4)",
+                description: "Click 'Start review' to view your quiz.",
+                image: slideSets.forms.slide3
+            },
+            {
+                title: "Sharing your Quiz (4/4)",
+                description: "Click 'Collect responses' to share your quiz.",
+                image: slideSets.forms.slide4
+            }
+        ],
+
+        blooket: [
+            {
+                title: "<a class='generating-text'>Generating quiz</a>",
+                description: "Please allow up to 60 seconds for your quiz to download.<br>This quick guide will show you how to import your quiz into Blooket.",
+                image: slideSets.blooket.slide0
+            },
+            {
+                title: "Opening Blooket (1/5)",
+                description: "Click <a href='https://www.blooket.com/' target='_blank'>here</a> to open Blooket. Sign into your account.",
+                image: slideSets.blooket.slide1
+            },
+            {
+                title: "Creating a Question Set (2/5)",
+                description: "Click <strong>Create</strong> → <strong>CSV Upload</strong>.",
+                image: slideSets.blooket.slide2
+            },
+            {
+                title: "Uploading your Quiz (3/5)",
+                description: "Enter your set details and click <strong>Create Set</strong>.",
+                image: slideSets.blooket.slide3
+            },
+            {
+                title: "Importing your Questions (4/5)",
+                description: "Click <strong>Upload CSV</strong> and select your downloaded file.",
+                image: slideSets.blooket.slide4
+            },
+            {
+                title: "Save your Set (5/5)",
+                description: "Check your questions and click <strong>Save Set</strong>. You will see your quiz in your account!",
+                image: slideSets.blooket.slide5
+            }
+        ],
+
+        gimkit: [
+            {
+                title: "<a class='generating-text'>Generating quiz</a>",
+                description: "Please allow up to 60 seconds for your quiz to download.<br>This quick guide will show you how to import your quiz into Gimkit.",
+                image: slideSets.gimkit.slide0
+            },
+            {
+                title: "Opening Gimkit (1/5)",
+                description: "Click <a href='https://www.gimkit.com/' target='_blank'>here</a> to open Gimkit. Sign into your account.",
+                image: slideSets.gimkit.slide1
+            },
+            {
+                title: "Creating a Question Set (2/5)",
+                description: "Click <strong>New Kit</strong> and fill in your quiz details.",
+                image: slideSets.gimkit.slide2
+            },
+            {
+                title: "Importing your Quiz (3/5)",
+                description: "Click <strong>Import from Spreadsheet</strong>.",
+                image: slideSets.gimkit.slide3
+            },
+            {
+                title: "Importing your Quiz (4/5)",
+                description: "Click <strong>Upload File</strong> and select your Gimkit CSV.",
+                image: slideSets.gimkit.slide4
+            },
+            {
+                title: "Save your Set (5/5)",
+                description: "Check your questions and click <strong>All Done</strong>. You will see your quiz in your account!",
+                image: slideSets.gimkit.slide5
+            }
+        ]
+    };
+
+    const slideState = {
+        forms: 0,
+        blooket: 0,
+        gimkit: 0
+    };
+
+    function updateSlide(type) {
+        const slide = slides[type][slideState[type]];
+
+        $(`#${type}ModalTitle`).html(slide.title);
+        $(`#${type}ModalDesc`).html(slide.description);
+        $(`#${type}ModalImg`).attr("src", slide.image);
+
+        // Previous button
+        slideState[type] === 0
+            ? $(`#${type}-previous-slide`).hide()
+            : $(`#${type}-previous-slide`).show();
+
+        // Next button
+        slideState[type] === slides[type].length - 1
+            ? $(`#${type}-next-slide`).hide()
+            : $(`#${type}-next-slide`).show();
+    }
+
+
+    // Quiz type handler
+    function handleQuizType(type, maxChoices = 4) {
+        const choices = parseInt($('input[name="no_of_choices"]').val());
+
+        if (choices > maxChoices) {
+            alert(`${type.charAt(0).toUpperCase() + type.slice(1)} supports a maximum of ${maxChoices} answer choices.`);
+            return;
         }
-    });
 
-    // Open quiz type modal
-    $('#generate-quiz').on('click', function() {
+        $('#quiz_type_field').val(type);
+        $('form').submit();
 
-        $('#quiz-type').show();
+        $('#quiz-type').fadeOut(300, function () {
+            $(`#${type}-quiz`).fadeIn(300);
+        });
+    }
 
+
+    // Button bindings
+    $('#generate-quiz').on('click', function () {
+
+        // Hide all quiz guides
         $('#forms-quiz').hide();
         $('#worksheet-quiz').hide();
         $('#presentation-quiz').hide();
         $('#blooket-quiz').hide();
         $('#gimkit-quiz').hide();
 
-        formsSlide = 0;
-        blooketSlide = 0;
-        gimkitSlide = 0;
+        // Show quiz type selector
+        $('#quiz-type').show();
 
-        updateFormsSlide();
-        updateBlooketSlide();
-        updateGimkitSlide();
+        // Reset slides
+        slideState.forms = 0;
+        slideState.blooket = 0;
+        slideState.gimkit = 0;
+
+        updateSlide("forms");
+        updateSlide("blooket");
+        updateSlide("gimkit");
 
         $modal.addClass('show');
     });
 
-    // Use as Forms
-    $('#use-as-forms').on('click', function () {
-        $('#quiz_type_field').val('forms'); // Pass into views
-        $('form').submit(); // Submit form using hidden input
-        $('#quiz-type').fadeOut(300, function () {
-            $('#forms-quiz').fadeIn(300);
+    $('#use-as-forms').on('click', () => handleQuizType("forms", 10));
+    $('#use-as-worksheet').on('click', () => handleQuizType("worksheet", 10));
+    $('#use-as-presentation').on('click', () => handleQuizType("presentation", 10));
+    $('#use-as-blooket').on('click', () => handleQuizType("blooket", 4));
+    $('#use-as-gimkit').on('click', () => handleQuizType("gimkit", 4));
+
+    // Slide navigation
+    ['forms', 'blooket', 'gimkit'].forEach(type => {
+        $(`#${type}-previous-slide`).on('click', () => {
+            slideState[type]--;
+            updateSlide(type);
+        });
+
+        $(`#${type}-next-slide`).on('click', () => {
+            slideState[type]++;
+            updateSlide(type);
         });
     });
 
-    // Use as Worksheet
-    $('#use-as-worksheet').on('click', function () {
-        $('#quiz_type_field').val('worksheet');
-        $('form').submit();
-        $('#quiz-type').fadeOut(300, function () {
-            $('#worksheet-quiz').fadeIn(300);
-        });
-    });
 
-    // Use as Presentation
-    $('#use-as-presentation').on('click', function () {
-        $('#quiz_type_field').val('presentation');
-        $('form').submit();
-        $('#quiz-type').fadeOut(300, function () {
-            $('#presentation-quiz').fadeIn(300);
-        });
-    });
-
-    // Use as Blooket
-    $('#use-as-blooket').on('click', function () {
-
-        // Blooket only supports up to 4 choices
-        const choices = parseInt($('input[name="no_of_choices"]').val());
-        if (choices > 4) {
-            alert("Blooket supports a maximum of 4 answer choices. Please reduce the number of choices before generating your quiz.");
-            return;
-        }
-
-        $('#quiz_type_field').val('blooket');
-        $('form').submit();
-        $('#quiz-type').fadeOut(300, function () {
-            $('#blooket-quiz').fadeIn(300);
-        });
-
-    });
-
-    // Use as Gimkit
-    $('#use-as-gimkit').on('click', function () {
-
-        // Gimkit only supports up to 4 choices
-        const choices = parseInt($('input[name="no_of_choices"]').val());
-        if (choices > 4) {
-            alert("Gimkit supports a maximum of 4 answer choices. Please reduce the number of choices before generating your quiz.");
-            return;
-        }
-
-        $('#quiz_type_field').val('gimkit');
-        $('form').submit();
-        $('#quiz-type').fadeOut(300, function () {
-            $('#gimkit-quiz').fadeIn(300);
-        });
-
-    });
-
-    // Forms slide navigation
-    $('#forms-previous-slide').on('click', function () {
-        formsSlide--;
-        updateFormsSlide();
-    });
-    $('#forms-next-slide').on('click', function () {
-        formsSlide++;
-        updateFormsSlide();
-    });
-
-    // Blooket slide navigation
-    $('#blooket-previous-slide').on('click', function () {
-        blooketSlide--;
-        updateBlooketSlide();
-    });
-    $('#blooket-next-slide').on('click', function () {
-        blooketSlide++;
-        updateBlooketSlide();
-    });
-
-    // Gimkit slide navigation
-    $('#gimkit-previous-slide').on('click', function () {
-        gimkitSlide--;
-        updateGimkitSlide();
-    });
-    $('#gimkit-next-slide').on('click', function () {
-        gimkitSlide++;
-        updateGimkitSlide();
-    });
-
-
-    // Drag-drop box for uploading files
+    // File upload
     const $dropZone = $("#drop-zone");
     const $fileInput = $dropZone.find("input[type='file']");
     const $fileNameDisplay = $("#file-name");
 
     function updateFileName() {
-        const files = $fileInput[0].files;
-        if (files.length > 0) {
-        $fileNameDisplay.text("Selected file: " + files[0].name);
-        } else {
-        $fileNameDisplay.text("");
-        }
+        const file = $fileInput[0].files[0];
+        $fileNameDisplay.text(file ? `Selected file: ${file.name}` : "");
     }
 
-    $dropZone.on("dragenter dragover", function (e) {
+    $dropZone.on("dragenter dragover", e => {
         e.preventDefault();
-        e.stopPropagation();
         $dropZone.addClass("dragover");
     });
 
-    $dropZone.on("dragleave drop", function (e) {
+    $dropZone.on("dragleave drop", e => {
         e.preventDefault();
-        e.stopPropagation();
         $dropZone.removeClass("dragover");
     });
 
-    $dropZone.on("drop", function (e) {
-        const files = e.originalEvent.dataTransfer.files;
-        $fileInput[0].files = files;
+    $dropZone.on("drop", e => {
+        $fileInput[0].files = e.originalEvent.dataTransfer.files;
         updateFileName();
     });
 
     $fileInput.on("change", updateFileName);
 
-    // Generating... text animation
+
+    // Generating... animation
     let dots = 0;
-    setInterval(function () {
-        dots = (dots + 1) % 4; // cycles 0–3 dots
-        let text = "Generating quiz" + ".".repeat(dots);
-        $(".generating-text").text(text);
+    setInterval(() => {
+        dots = (dots + 1) % 4;
+        $(".generating-text").text("Generating quiz" + ".".repeat(dots));
     }, 700);
 
 });
